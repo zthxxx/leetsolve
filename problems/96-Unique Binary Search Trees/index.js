d8feb9b4b41ebb5e3e9cae9336aba70fd575272c
zthxxx <zth_9451@qq.com>
2017-11-30 23:06:33 +0800

    feat: add problem-96 Unique Binary Search Trees

---
+++ problems/96-Unique Binary Search Trees/index.js
@@ -0,0 +1,33 @@
+require('../../libs/runDirect')
+
+/**
+ * [0] = 1
+ * [1] = 1
+ * [2] = ([0] x [1]) x 2 = 2
+ * [3] = ([0] x [2]) x 2 + [1] x [1] = 5
+ * [4] = ([0] x [3] + [1] x [2]) x 2 = 14
+ * [5] = ([0] x [4] + [1] x [3]) x 2 + [2] x [2] = 42
+ * [6] = ([0] x [5] + [1] x [4] + [2] x [3]) x 2 = 132
+ * 一颗树的可能情况等于这颗树左右可能情况的乘积
+ * 一棵树的左子树节点数可能情况从 0 ~ n - 1
+ * 同理右侧也一样
+ * @param {number} n
+ * @param {number[]} cache
+ * @return {number}
+ */
+let numTrees = function (n) {
+  let counts = [1, 1]
+  function multiCount (n) {
+    if (n === 0 || n === 1) return 1
+    multiCount(n - 1)
+    let total = 0
+    for (let i = 0; i < n; i++) {
+      total += counts[i] * counts[n - 1 - i]
+    }
+    counts.push(total)
+    return total
+  }
+  return multiCount(n)
+}
+
+module.exports = numTrees
---
+++ problems/96-Unique Binary Search Trees/testcase.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    input: [0],
+    expect: 1
+  },
+  {
+    input: [1],
+    expect: 1
+  },
+  {
+    input: [2],
+    expect: 2
+  },
+  {
+    input: [3],
+    expect: 5
+  },
+  {
+    input: [4],
+    expect: 14
+  },
+  {
+    input: [5],
+    expect: 42
+  },
+  {
+    input: [6],
+    expect: 132
+  },
+  {
+    input: [7],
+    expect: 429
+  },
+  {
+    input: [8],
+    expect: 1430
+  },
+  {
+    input: [9],
+    expect: 4862
+  },
+  {
+    input: [19],
+    expect: 1767263190
+  }
+]
